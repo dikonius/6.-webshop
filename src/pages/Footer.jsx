46d4a1e936429e116b58255eaff9a29b6ad3d457
dikonius <eazy777er@gmail.com>
2025-05-07 12:41:48 +0200

    fixed basic footer and header for HashRouter

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,36 @@
+const Footer = () => {
+    
+  
+  
+  
+    return (
+      <footer className="footer-container">
+
+            <NavLink to="/" className="navlink-footer">
+                <button className="footer-btn">Home</button>
+            </NavLink>
+
+            <NavLink to="/pages/games/" className="navlink-footer">
+                <button className="footer-btn">Games</button>
+            </NavLink>
+
+            <NavLink to="/pages/consoles/" className="navlink-footer">
+                <button className="footer-btn">Consoles</button>
+            </NavLink>
+
+            <div className="socials-container">
+              <a className="social-links" href="https://x.com/" target="_blank"><img className="social-logos" src="/src/assets/xcom-logo.jpeg" alt="X.com logo"/></a>
+              <a className="social-links" href="https://www.instagram.com/" target="_blank"><img className="social-logos" src="/src/assets/inst-logo.png" alt="Instagram logo"/></a>
+              <a className="social-links" href="https://www.facebook.com/" target="_blank"><img  className="social-logos" src="/src/assets/face-logo.png.png" alt="Facebook logo"/></a>
+			      </div>
+         
+            <NavLink to="pages/cart/:cartId?" className="navlink-footer">
+            <button className="admin-btn">Admin</button>
+            </NavLink>
+            
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+
+
+	return (
+		<main>
+            <img src="/images/logo.png" alt="Switch Again Logo" className="hero-logo" />
+			<p className="welcome-description">Welcome to Switch Again! Your<span className="key-word"> Online Second-Hand </span>Nintendo Switch Store!</p>
+			<p className="welcome-description"><span className="key-word"> Save big </span>by buying pre-owned and enjoy the same amazing gaming experience as with brand-new products!</p>
+			<p className="welcome-description">All our games and consoles are expertly restored, thoroughly cleaned, and in<span className="key-word"> perfect condition. </span></p>
+			<p className="welcome-description">Enjoy<span className="key-word"> free shipping </span>on every order, and everything you see on our website is in stock and ready to ship!</p>
+		</main>
+	)
+}
+export default Home
